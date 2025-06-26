74bd64fe6175ff0c1b117cdc3a42c860609f767d
David Pitta <davidfrpitta@gmail.com>
2025-06-26 15:24:22 -0300

    ajustes

--- src/app/pages/pedido/service/pedido.service.ts
+++ src/app/pages/pedido/service/pedido.service.ts
@@ -1,18 +1,28 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Observable, of } from 'rxjs';
 import { Pedido } from '../models/pedido';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PedidoService {
-
   private readonly baseUrl = 'https://menu360-backend-production.up.railway.app';
+  private isBrowser: boolean;
 
-  constructor(protected httpClient: HttpClient) { }
+  constructor(
+    protected httpClient: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
   listaPedido(): Observable<Pedido[]> {
+    if (!this.isBrowser) {
+      return of([]);
+    }
+
     const restauranteId = localStorage.getItem('restauranteId');
     const url = `${this.baseUrl}/pedido?id_restaurante=${restauranteId}`;
     return this.httpClient.get<Pedido[]>(url);
